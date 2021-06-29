6809dc8df4ccb82762a24c7ef46f2df6e1e311dc
Elizaveta Ozerova <e.ozerova@adsterra.com>
2021-06-29 18:40:35 +0300

    рефакторинг

--- src/components/StandAlone.vue
+++ src/components/StandAlone.vue
@@ -1,6 +1,7 @@
 <template>
   <p>Hi, Michail!</p>
-  <p>This is a standalone component loaded asynchronously!</p>
+  <p>This is a standalone component loaded asynchronously.</p>
+  <p>Have a nice day!</p>
 </template>
 
 <script>
